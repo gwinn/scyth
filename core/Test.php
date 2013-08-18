fe9515e5b7cd56271c9962ef73e0736268897ae4
Alex Lushpai <alex@lushpai.org>
2013-08-18 04:40:37 +0400

    test

---
+++ core/Test.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ *  Test
+ *
+ *  PHP Version 5.4
+ *
+ *  @category Core
+ *  @package  Scyth
+ *  @author   Alex Lushpai <alex@lushpai.org>
+ *  @license  MIT http://opensource.org/licenses/MIT
+ *  @link     http://scyth.jtforce.com/guide/core/cypher.html
+ *
+ */
+
+
+/**
+ *  Test class
+ *
+ *  @category Core
+ *  @package  Scyth
+ *  @author   Alex Lushpai <alex@lushpai.org>
+ *  @license  MIT http://opensource.org/licenses/MIT
+ *  @link     http://scyth.jtforce.com/guide/index.html
+ *
+ */
+
+class Test {
+
+    /** Test Method
+     *
+     * @return void
+     */
+    public function test()
+    {
+        echo 1;
+    }
+
+}
